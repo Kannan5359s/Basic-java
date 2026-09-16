f69c06ecc8449a2fb80b5ba175e741df9f1e59ce
Kannan M <mkannan0207@gmail.com>
2026-09-16 22:09:13 +0530

    Time: 3 ms (85.19%) | Memory: 48 MB (83.69%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
@@ -1,23 +1,13 @@
 class Solution {
-    public static boolean binarysec(int[] arr,int num){
-        int low=0,high=arr.length-1;
-        while(low<=high){
-            int mid=low+((high-low)/2);
-            if(arr[mid]==num) return true;
-            if(arr[mid] > num) high=mid-1;
-            else low=mid+1;
-        }
-        return false;
-
-    }
     public boolean searchMatrix(int[][] matrix, int target) {
-        int m=matrix[0].length;
-        int n=matrix.length;
-        for(int i=0;i<n;i++){
-            if(binarysec(matrix[i],target)){
+        int i=0;
+        int j=matrix[0].length-1;
+        while(i<matrix.length & j>=0){
+            if(matrix[i][j] == target ){
                 return true;
             }
-
+            if(matrix[i][j] > target) j--;
+            else i++;
         }
         return false;
     }
